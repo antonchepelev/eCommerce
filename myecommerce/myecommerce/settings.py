ebd4d411c30e17aa42449901f2c379ca61daa0b0
Anton Chepelev <131076039+antonchepelev@users.noreply.github.com>
2023-08-01 11:53:33 -0400

    Update settings.py

--- myecommerce/myecommerce/settings.py
+++ myecommerce/myecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-e+b-a6ub*9_=wcyav%gk=^rl_ct(=-@1290o*xktyb64y)qucd"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
